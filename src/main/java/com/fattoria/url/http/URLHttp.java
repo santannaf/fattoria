cbf99a6462db8e2637541d30bedbbfdd00625756
thalesfariassantanna <thales.farias.santanna@gmail.com>
2019-07-09 23:45:13 -0300

    incluindo validacao url

--- src/main/java/com/fattoria/url/http/URLHttp.java
+++ src/main/java/com/fattoria/url/http/URLHttp.java
@@ -5,6 +5,7 @@ import com.fattoria.url.usecase.URLUsecase;
 import com.fattoria.url.usecase.data.response.URLUsecaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.constraintvalidators.hv.URLValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.io.IOException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,6 +35,8 @@ public class URLHttp {
     @PostMapping(path = "/url")
     public ResponseEntity<URLUsecaseResponse> createUrl(@Valid @RequestBody URLHttpRequest body) {
         try {
+            if (!isValid(body.getUrlOriginal())) throw new Exception("URL Inválida !");
+
             String randomChar = getRandomChars();
             String urlModificada = "http://localhost:8080/url/" + randomChar;
             shortenUrlList.put(randomChar, body);
@@ -46,7 +50,7 @@ public class URLHttp {
             return new ResponseEntity<>(response, HttpStatus.CREATED);
         } catch (Exception error) {
             log.error("Erro ao criar um contato > " + error.getMessage());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "URL não foi criada > " + error.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "URL não foi criada > " + error.getMessage() + " body: " + body.getUrlOriginal());
         }
     }
 
@@ -56,5 +60,14 @@ public class URLHttp {
         for (int i = 0; i < 7; i++)
             randomStr += possibleChars.charAt((int) Math.floor(Math.random() * possibleChars.length()));
         return randomStr;
+    }
+
+    public static boolean isValid(String url) {
+        try {
+            new URL(url).toURI();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
